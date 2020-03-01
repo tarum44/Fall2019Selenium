2dc43d762f1ee834afb0186fe3e8096218a2b703
muratkilinc <tarum44@gmail.com>
2020-03-01 15:16:00 -0600

    final version

--- src/test/java/com/automation/tests/day2/QuitvsClose.java
+++ src/test/java/com/automation/tests/day2/QuitvsClose.java
@@ -1,4 +1,23 @@
 package com.automation.tests.day2;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class QuitvsClose {
+    public static void main(String[] args) throws Exception{
+        WebDriverManager.chromiumdriver().setup();
+
+        WebDriver driver=new ChromeDriver();
+
+
+        driver.get("http://practice.cybertekschool.com/open_new_tab");
+
+        Thread.sleep(4000);
+
+//        driver.close(); will close only one window
+        driver.quit();
+
+
+    }
 }
