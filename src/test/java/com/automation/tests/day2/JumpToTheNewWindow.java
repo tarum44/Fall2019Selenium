878c221f1fede8da528b6d1f92a216b121051961
muratkilinc <tarum44@gmail.com>
2020-03-01 16:06:24 -0600

    added methods

--- src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
+++ src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
@@ -1,4 +1,66 @@
 package com.automation.tests.day2;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Set;
+
 public class JumpToTheNewWindow {
+    public static void main(String[] args) throws Exception {
+        WebDriverManager.chromiumdriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/open_new_tab");
+
+        Thread.sleep(5000);
+
+        // every windows has some id, this id calls window handle
+        // based on window handle, we can switch in between windows
+        String windowHandle = driver.getWindowHandle();
+
+        System.out.println(windowHandle);
+
+        // getWindowHandles() - returns id's of all currently opened windows
+        // Set - doesn't allow duplicates
+
+        Set<String> windowHandles = driver.getWindowHandles();
+
+        System.out.println(windowHandle);
+        System.out.println("BEFORE SWITCH :" + driver.getCurrentUrl());
+        // since we have all windows
+        // and we know id of original window
+        // we can say switch to something that is not equals to old window id
+        for (String windowId : windowHandles) {
+            // if it's not an old window, then switch
+            if (!windowId.equals(windowHandle)) {
+                // to jump to the new window
+                driver.switchTo().window(windowId);
+            }
+        }
+
+        System.out.println("AFTER SWITCH :" + driver.getCurrentUrl());
+
+//        driver.close();
+
+
+    }
+
+    /*
+     *    A method to move if we have more than two tabs
+     */
+
+
+    public static void swithcToWindowBasedonTitle(String pageTitle, WebDriver driver) {
+        Set<String> windows = driver.getWindowHandles();
+        for (String window : windows) {
+            driver.switchTo().window(window);
+            if (driver.getTitle().equals(pageTitle)) {
+                break;
+            }
+
+
+        }
+    }
 }
