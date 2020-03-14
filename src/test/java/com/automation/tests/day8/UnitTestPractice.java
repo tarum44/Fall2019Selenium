dcf49064c312f8d34410ff9208ce363dd0c966b4
muratkilinc <tarum44@gmail.com>
2020-03-14 12:39:34 -0500

    SearchTests

--- src/test/java/com/automation/tests/day8/UnitTestPractice.java
+++ src/test/java/com/automation/tests/day8/UnitTestPractice.java
@@ -1,4 +1,51 @@
 package com.automation.tests.day8;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class UnitTestPractice {
+   public static void main(String[] args){
+       String expected="cba";
+       String toReverse="abc";
+       String actual=reverseString(toReverse);
+       verifyEquals(expected,actual);
+   }
+
+   // annotation
+   @Test(description = "Verify if method can reverse a string")
+   public void test(){
+       String expected="elpa";
+       String actual=reverseString("apple");
+       // it is coming from testng, junit also has this class
+       // you can compare any data types here: strings, primitives, arrays, objects
+       // to verify if expected result is equals to actual
+       Assert.assertEquals(actual,expected);
+   }
+
+   @Test(description = "Verify if method can reverse a string")
+   public void test2(){
+       String expected="rac";
+       String actual=reverseString("car");
+       Assert.assertEquals(actual,expected);
+   }
+
+   public static  boolean verifyEquals(String expected,String actual){
+       if(expected.equals(actual)){
+           System.out.println("TEST PASSED");
+           return true;
+       }else{
+           System.out.println("TEST FAILED");
+           System.out.println("Expected: "+expected);
+           System.out.println("Actual: "+actual);
+           return false;
+       }
+   }
+
+    public static String reverseString(String str){
+        String reversed="";
+        for(int i=str.length()-1;i>=0;i--){
+            reversed += str.charAt(i);
+        }
+        return reversed;
+    }
 }
